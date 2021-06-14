1177b19de3c780f73e45c357f1fa12b2ba81ff0a
fernandoAvargas <fernando.avargas.fv@gmail.com>
2021-06-14 01:20:49 -0300

    Base Class Create and Notify adjusts

---
+++ DDD_2020/Entities/Entities/Base.cs
@@ -0,0 +1,16 @@
+﻿using Entities.Notifications;
+using System.ComponentModel.DataAnnotations;
+
+namespace Entities
+{
+    public class Base : Notifies
+    {
+
+        [Display(Name = "Código")]
+        public int Id { get; set; }
+
+        [Display(Name = "Nome")]
+        public string Nome { get; set; }
+
+    }
+}
--- DDD_2020/Entities/Notifications/Notifies.cs
+++ DDD_2020/Entities/Notifications/Notifies.cs
@@ -4,11 +4,11 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Entities.Notifications
 {
-    public class Notify
+    public class Notifies
     {
-        public Notify()
+        public Notifies()
         {
-            Notitycoes = new List<Notify>();
+            Notitycoes = new List<Notifies>();
         }
 
         [NotMapped]
@@ -18,14 +18,14 @@ namespace Entities.Notifications
         public string mensagem { get; set; }
 
         [NotMapped]
-        public List<Notify> Notitycoes;
+        public List<Notifies> Notitycoes;
 
         public bool ValidarPropriedadeString(string valor, string nomePropriedade)
         {
 
             if (string.IsNullOrWhiteSpace(valor) || string.IsNullOrWhiteSpace(nomePropriedade))
             {
-                Notitycoes.Add(new Notify
+                Notitycoes.Add(new Notifies
                 {
                     mensagem = "Campo Obrigatório",
                     NomePropriedade = nomePropriedade
@@ -42,7 +42,7 @@ namespace Entities.Notifications
         {
             if (valor < 1 || string.IsNullOrWhiteSpace(nomePropriedade))
             {
-                Notitycoes.Add(new Notify
+                Notitycoes.Add(new Notifies
                 {
                     mensagem = "Campo Obrigatório",
                     NomePropriedade = "nomePropriedade"
